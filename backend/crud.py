c12fe6eb1e31bf8357448d35be0f54aebe838fc1
bansvai <bansalvaibhav506@gmail.com>
2024-07-18 13:45:14 +0530

    Adding operation to get a random user from the database

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,4 @@
+import random
 from sqlalchemy.orm import Session
 from .models import User
 
@@ -7,3 +8,10 @@ def create_user(db: Session, user_data):
     db.commit()
     db.refresh(user)
     return user
+
+def get_random_user(db: Session):
+    user_count = db.query(User).count()
+    if user_count == 0:
+        return None
+    random_index = random.randint(0, user_count - 1)
+    return db.query(User).offset(random_index).limit(1).first()
